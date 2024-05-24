24024fc105a0779275beac783a82207d2fa1df67
Mrbysco <Mrbysco@users.noreply.github.com>
2024-05-24 21:31:46 +0200

    Re-enable CraftTweaker support

--- src/main/java/com/mrbysco/structurecompass/compat/crafttweaker/StructureStageCT.java
+++ src/main/java/com/mrbysco/structurecompass/compat/crafttweaker/StructureStageCT.java
@@ -1,16 +1,16 @@
-//package com.mrbysco.structurecompass.compat.crafttweaker;
-//
-//import com.blamejared.crafttweaker.api.CraftTweakerAPI;
-//import com.blamejared.crafttweaker.api.annotation.ZenRegister;
-//import jdk.jfr.Name;
-//import net.minecraft.resources.ResourceLocation;
-//import org.openzen.zencode.java.ZenCodeType;
-//
-//@ZenRegister
-//@Name("mods.structurecompass.staging")
-//public class StructureStageCT {
-//	@ZenCodeType.Method
-//	public static void setStructureStages(ResourceLocation containerName, String... stages) {
-//		CraftTweakerAPI.apply(new ActionSetStructureStaging(containerName, stages));
-//	}
-//}
+package com.mrbysco.structurecompass.compat.crafttweaker;
+
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import jdk.jfr.Name;
+import net.minecraft.resources.ResourceLocation;
+import org.openzen.zencode.java.ZenCodeType;
+
+@ZenRegister
+@Name("mods.structurecompass.staging")
+public class StructureStageCT {
+	@ZenCodeType.Method
+	public static void setStructureStages(ResourceLocation containerName, String... stages) {
+		CraftTweakerAPI.apply(new ActionSetStructureStaging(containerName, stages));
+	}
+}
